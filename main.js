b9c9449c8f214351ca5a5661c114375f267a237f
sebbmn <sebbmn@users.noreply.github.com>
2022-05-24 11:36:07 +0200

    feat: setup title & display

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import './style.css'
 
 document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+  <h1>Infinite z Scroll</h1>
+  <div class="content">
+    <div class="scrollable-div"></div>
+  </div>
 `
