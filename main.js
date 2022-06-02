487f2605eaaf3485c77dd31ddee4cb3a05d0c826
sebbmn <sebbmn@users.noreply.github.com>
2022-06-02 17:02:35 +0200

    feat: style w/o padding/margin & overflow hidden

--- main.js
+++ main.js
@@ -5,11 +5,6 @@ const el = document.querySelector('#scrollable-div')
 
 const zoom = (e) => {
   scale += e.deltaY * -0.01
-
-  // Restrict scale
-  scale = Math.min(Math.max(.125, scale), 4)
-
-  // Apply scale transform
   el.style.transform = `scale(${scale})`
 }
 
