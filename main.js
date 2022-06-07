0af5e22fb4fb610b69909f2ffa0276c3edab1ca5
sebbmn <sebbmn@users.noreply.github.com>
2022-06-07 11:37:56 +0200

    feat: rescale recusively all elements

--- main.js
+++ main.js
@@ -4,13 +4,17 @@ const el = document.querySelector('#scrollable-div')
 
 const zoom = (e) => {
   const scale = 1 + (e.deltaY * 0.01)
-  rescale(scale, el)
+  let element = el.children[0]
+
+  while (element) {
+    rescale(scale, element)
+    element = element.children[0]
+  }
 }
 
 const rescale = (scale, element) => {
-  element.style.width =  `${Math.trunc(scale * element.clientWidth)}px`
-  element.style.height =  `${Math.trunc(scale * element.clientHeight)}px`
+  element.style.width =  `${Math.round(scale * element.clientWidth)}px`
+  element.style.height =  `${Math.round(scale * element.clientHeight)}px`
 }
 
 document.addEventListener('wheel', zoom)
-
