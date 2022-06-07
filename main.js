d29433039e61c4bf4b934ad1c41f4a6ad592ccad
sebbmn <sebbmn@users.noreply.github.com>
2022-06-07 11:17:05 +0200

    refactor: extract scale function from zoom

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 import './style.css'
 
-let scale = 1
 const el = document.querySelector('#scrollable-div')
 
 const zoom = (e) => {
-  scale += e.deltaY * -0.01
-  el.style.transform = `scale(${scale})`
+  const scale = 1 + (e.deltaY * 0.01)
+  rescale(scale, el)
+}
+
+const rescale = (scale, element) => {
+  element.style.width =  `${Math.trunc(scale * element.clientWidth)}px`
+  element.style.height =  `${Math.trunc(scale * element.clientHeight)}px`
 }
 
 document.addEventListener('wheel', zoom)
