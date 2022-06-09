1328940cd3777286242a48ae244602c63ada19d2
sebbmn <sebbmn@users.noreply.github.com>
2022-06-09 10:26:54 +0200

    fix: preserve ratio&scale while zooming

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
 import './style.css'
 
 const el = document.querySelector('#scrollable-div')
+const ratio = 9 / 16
 
 const zoom = (e) => {
   const scale = 1 + (e.deltaY * 0.01)
+
   let element = el.children[0]
+  let width = Math.round(scale * element.clientWidth)
+  let height = Math.round(width * ratio)
 
   while (element) {
-    rescale(scale, element)
+    resize(element, width, height)
     element = element.children[0]
+    width = width / 5
+    height = Math.round(width * ratio)
   }
 }
 
-const rescale = (scale, element) => {
-  element.style.width =  `${Math.round(scale * element.clientWidth)}px`
-  element.style.height =  `${Math.round(scale * element.clientHeight)}px`
+const resize = (element, width, height) => {
+  element.style.width =  `${width}px`
+  element.style.height =  `${height}px`
 }
 
 document.addEventListener('wheel', zoom)
