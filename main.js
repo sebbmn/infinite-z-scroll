70a25b0a6fd77098ce4df9a10531e767091e5e56
sebbmn <sebbmn@users.noreply.github.com>
2022-06-10 12:05:57 +0200

    feat: init layout dynamically

--- main.js
+++ main.js
@@ -3,9 +3,32 @@ import './style.css'
 const el = document.querySelector('#scrollable-div')
 const ratio = 9 / 16
 
+//window.innerWidth
+//window.innerHeight
+
+const initLayout = () => {
+  let root = document.querySelector('#scrollable-div')
+  appendChild(3, root)
+}
+
+const appendChild = (depth, parent) => {
+  const divItem = document.createElement('div')
+  divItem.className = 'scrollable-div-item'
+  divItem.style = `width:${50}px;height:${50}px`
+
+  parent.append(divItem)
+  depth--
+
+  if (depth > 0) {
+    parent = parent.children[0]
+    appendChild(depth, parent)
+  }
+}
+
 const zoom = (e) => {
   const scale = 1 + (e.deltaY * 0.01)
   const root = el.children[0]
+
   const width = Math.round(scale * root.clientWidth)
   const height = Math.round(width * ratio)
 
@@ -32,3 +55,4 @@ const resize = (element, width, height) => {
 }
 
 document.addEventListener('wheel', zoom)
+initLayout()
