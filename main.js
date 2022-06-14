a92c531f67f32e0b7af1f609121d5f53f82e193f
sebbmn <sebbmn@users.noreply.github.com>
2022-06-14 12:09:45 +0200

    refactor: impr. naming. Prepare scale up/down.

--- main.js
+++ main.js
@@ -3,18 +3,17 @@ import './style.css'
 const ratio = 9 / 16
 
 const initLayout = () => {
-  const root = document.querySelector('#scrollable-div')
-  const topParent = document.createElement('div')
   const windowRatio = window.innerHeight / window.innerWidth
+  const root = document.querySelector('#scrollable-div')
 
   const startWidth = ratio < windowRatio ? window.innerHeight / ratio : window.innerWidth
   const startHeight = ratio < windowRatio ? window.innerHeight : window.innerWidth * ratio 
 
+  const topParent = document.createElement('div')
   topParent.className = 'scrollable-div-item'
-  topParent.style = `width:${startWidth}px;height:${startHeight}px`
+  resize(topParent, startWidth, startHeight)
 
   root.append(topParent)
-
   appendChild(3, topParent)
 }
 
@@ -24,7 +23,7 @@ const appendChild = (depth, parent) => {
   const height = Math.round(width * ratio)
 
   divItem.className = 'scrollable-div-item'
-  divItem.style = `width:${width}px;height:${height}px`
+  resize(divItem, width, height)
 
   parent.append(divItem)
   depth--
@@ -38,14 +37,22 @@ const appendChild = (depth, parent) => {
 const zoom = (e) => {
   const scale = 1 + (e.deltaY * 0.01)
   const root = document.querySelector('#scrollable-div').children[0]
+  const topParent = root.children[0]
 
   const width = Math.round(scale * root.clientWidth)
   const height = Math.round(width * ratio)
 
-  rescaleTree(root, width, height)
+  if (topParent.clientWidth > window.innerWidth && topParent.clientHeight > window.innerHeight) {
+    scaleUpTree(root)
+  }
+  if (width < window.innerWidth && height < window.innerHeight) {
+    scaleDownTree(root)
+  }
+
+  resizeTree(root, width, height)
 }
 
-const rescaleTree = (element, width, height) => {
+const resizeTree = (element, width, height) => {
   if (element) {
     resize(element, width, height)
 
@@ -53,7 +60,7 @@ const rescaleTree = (element, width, height) => {
     width = width / 5
     height = Math.round(width * ratio)
 
-    rescaleTree(element, width, height)
+    resizeTree(element, width, height)
   } else {
     return
   }
@@ -62,6 +69,16 @@ const rescaleTree = (element, width, height) => {
 const resize = (element, width, height) => {
   element.style.width =  `${width}px`
   element.style.height =  `${height}px`
+}
+
+const scaleUpTree = (root) => {
+  // TODO: scale up logic
+  console.log('up', root)
+}
+
+const scaleDownTree = (root) => {
+  // TODO: scale down logic
+  console.log('down', root)
 }
 
 document.addEventListener('wheel', zoom)
